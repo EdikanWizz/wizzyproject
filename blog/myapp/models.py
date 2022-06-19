949dee284b64d92a9e2062c96c759160cc1449e2
EdikanWizz <you@example.com>
2022-06-19 17:48:44 +0400

    git remote add origin https://github.com/EdikanWizz/mypython.git

---
+++ blog/myapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    title = models.CharField(max_length = 200)
+    text = models.TextField()
+    author = models.ForeignObject(User)
+    created_date = models.DateTimeField()
+    published_date = models.DateTimeField()
